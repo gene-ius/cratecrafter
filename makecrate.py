b7c0419081b669d5838adddf25e61c4523998b12
gene-ius <nartey.gene@gmail.com>
2023-03-19 01:37:59 -0400

    terminal colors for better download readability

--- makecrate.py
+++ makecrate.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import yt_dlp as youtube_dl
 
+from terminalstyles import bcolors
+
 #  HANDLING ARGUMENTS
 #  Get the folder name and Excel sheet name from command-line arguments
 if len(sys.argv) == 4:
@@ -14,9 +16,9 @@ elif len(sys.argv) == 3:
     folder_name = sys.argv[1]
     excel_sheet_name = sys.argv[2]
 else:
-    print('---------------- INVALID USAGE ------------------')
-    print('3 ARGUMENT Usage: python3 makecrate.py [crate_name] [excel_sheet_name].xlsx [output_directory ]')
-    print("2 ARGUMENT Usage: python3 makecrate.py [crate_name] [excel_sheet_name].xlsx ")
+    print( bcolors.FAIL + '---------------- OOPS! INVALID USAGE ------------------' + bcolors.ENDC )
+    print(bcolors.WARNING + 'FOR 3 ARGUMENT Use: python3 makecrate.py [crate_name] [excel_sheet_name].xlsx [output_directory ]' + bcolors.ENDC )
+    print(bcolors.WARNING + "FOR 2 ARGUMENT Use: python3 makecrate.py [crate_name] [excel_sheet_name].xlsx " + bcolors.ENDC )
     sys.exit()
 
 
@@ -28,36 +30,36 @@ df = pd.read_excel(excel_sheet_name)
 folder_path = os.path.join(output_dir , folder_name) if output_dir else os.path.join(folder_name)
 # Check if the folder already exists
 if os.path.exists(folder_path):
-    print('Folder already exists, songs will be downloaded to:', folder_path)
+    print(bcolors.OKBLUE + 'Folder already exists, songs will be downloaded to:' + bcolors.ENDC , folder_path)
 else:
-    print('Folder does not exist, creating new folder:', folder_path)
+    print(bcolors.OKBLUE + 'Folder does not exist, creating new folder:' + bcolors.ENDC, folder_path)
     os.makedirs(folder_path)
 
 # Loop through each row in the Excel sheet
 for index, row in df.iterrows():
     # Construct the YouTube search query using song title and author
     query = row['Song Title'] + ' ' + row['Author'] + ' audio'
-    print('Searching for:', query)
+    print(bcolors.HEADER + 'STARTING SEARCH FOR :', query + bcolors.ENDC )
     # Set the output paths to the folder where you want to save the songs to verify no duplicates
     output_path = os.path.join(folder_path, row['Song Title'] + ".mp3")
     output_filename = os.path.join(folder_path, row['Song Title'])
 
     print(type(row['Rename']))
     if type(row['Rename']) == str: #if rename value assigned in sheet
-        print("renaming to", row['Rename'])
+        print(bcolors.OKBLUE + "renaming to", row['Rename'] +  bcolors.ENDC)
         rename = str(row['Rename'])
         output_path = os.path.join(folder_path, rename + ".mp3")
         output_filename = os.path.join(folder_path, rename)
 
 
     if os.path.exists(output_path):
-         print('Song already exists, skipping download:', row['Song Title'] + ".mp3")
+         print( bcolors.OKBLUE + 'Song already exists, skipping download:', row['Song Title'] + ".mp3" + bcolors.ENDC )
     else:
         # Search for the song on YouTube
         ydl_opts = {'format': 'bestaudio/best', 'outtmpl': output_filename, 'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192',}],}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download(['ytsearch:' + query])
 
-        print('Song Successfully Downloaded:', row['Song Title'])
+        print(bcolors.OKCYAN + 'Song Successfully Downloaded:' + bcolors.ENDC , row['Song Title'])
 
-print(f'Successfully Downloaded all songs to {folder_name},! Time to MIX...')
+print( bcolors.OKGREEN + f'Successfully Downloaded all songs to {folder_name},! Time to MIX...' + bcolors.ENDC)
